ca47d1ddf9b362ca0a2b2dae535300588ebf17ae
SpiralP <spiral.palmer@gmail.com>
2019-05-14 17:56:48 -0700

    move ws sending to another thread so we can throttle

--- src/web_server/websocket_actor/mod.rs
+++ src/web_server/websocket_actor/mod.rs
@@ -17,7 +17,6 @@ pub struct WebSocketActor {
   root_path: Vec<String>,
   current_dir: Vec<String>,
   thread_sender: Option<Sender<ThreadControlMessage>>,
-  scanner: Option<FileSizeScanner>,
 }
 
 impl WebSocketActor {
@@ -26,7 +25,6 @@ impl WebSocketActor {
       root_path: get_components(root_path),
       current_dir: Vec::new(),
       thread_sender: None,
-      scanner: None,
     }
   }
 
@@ -46,16 +44,16 @@ fn send_directory_change(
   root_path: &[String],
   path: Vec<String>,
   tree: &Tree,
-  addr: &Addr<WebSocketActor>,
+  event_sender: &Sender<EventMessage>,
 ) -> (Vec<String>, Vec<String>) {
   let entries = get_directory_entries(root_path, path.clone(), &tree);
-  addr.do_send(TextMessage(
-    serde_json::to_string(&EventMessage::DirectoryChange {
+
+  event_sender
+    .send(EventMessage::DirectoryChange {
       path: path.clone(),
       entries: entries.clone(),
     })
-    .unwrap(),
-  ));
+    .unwrap();
 
   (
     path,
@@ -86,25 +84,34 @@ impl Actor for WebSocketActor {
     let (thread_sender, thread_receiver) = channel::unbounded();
     self.thread_sender = Some(thread_sender);
 
+    let (event_sender, event_receiver) = channel::unbounded();
+
     // send initial current directory entries
     self.change_dir(Vec::new());
 
-
-    let _thread = {
+    let _event_sender_thread = {
       let addr = ctx.address();
-      let root_path: Vec<String> = self.root_path.clone();
-
-      let (scanner, file_receiver) = FileSizeScanner::start(root_path.iter().collect());
-      self.scanner = Some(scanner);
 
       thread::spawn(move || {
+        for event in event_receiver {
+          addr.do_send(TextMessage(serde_json::to_string(&event).unwrap()));
+        }
+      })
+    };
+
+    let _thread = {
+      let root_path: Vec<String> = self.root_path.clone();
+
+      thread::spawn(move || {
+        let (_scanner, file_receiver) = FileSizeScanner::start(root_path.iter().collect());
+
         let mut tree = Tree::new();
 
         // wait for default current directory
         let (mut current_dir, mut subscribed_entries): (Vec<String>, Vec<String>) =
           match thread_receiver.recv().unwrap() {
             ThreadControlMessage::ChangeDirectory(path) => {
-              send_directory_change(&root_path, path, &tree, &addr)
+              send_directory_change(&root_path, path, &tree, &event_sender)
             }
           };
 
@@ -117,7 +124,7 @@ impl Actor for WebSocketActor {
                 ThreadControlMessage::ChangeDirectory(path) => {
                   println!("thread got dir change! {:?}", path);
 
-                  let (a, b) = send_directory_change(&root_path, path, &tree, &addr);
+                  let (a, b) = send_directory_change(&root_path, path, &tree, &event_sender);
                   current_dir = a;
                   subscribed_entries = b;
                 }
@@ -150,15 +157,12 @@ impl Actor for WebSocketActor {
 
               let size = tree.at(bap).unwrap().get_total_size();
 
-              addr.do_send(TextMessage(
-                serde_json::to_string(&EventMessage::SizeUpdate {
-                  entry: Entry::Directory {
-                    name: relative_name,
-                    size,
-                  }
-                })
-                .unwrap(),
-              ));
+              event_sender.send(EventMessage::SizeUpdate {
+                entry: Entry::Directory {
+                  name: relative_name,
+                  size,
+                }
+              }).unwrap();
             },
           }
         }
@@ -169,7 +173,7 @@ impl Actor for WebSocketActor {
             ThreadControlMessage::ChangeDirectory(path) => {
               println!("thread got dir change (after live update)! {:?}", path);
 
-              send_directory_change(&root_path, path, &tree, &addr);
+              send_directory_change(&root_path, path, &tree, &event_sender);
             }
           }
         }
